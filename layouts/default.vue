2eb7de4fb61189b4d84602d263c737af59065688
Hakamate <thomas.clement@viacesi.fr>
2020-07-19 18:38:41 +0200

    Test

--- layouts/default.vue
+++ layouts/default.vue
@@ -57,11 +57,11 @@
         <v-icon>mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-content>
+    <v-main>
       <v-container>
         <nuxt />
       </v-container>
-    </v-content>
+    </v-main>
     <v-navigation-drawer
       v-model="rightDrawer"
       :right="right"
@@ -103,14 +103,14 @@ export default {
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'Zip and UnZip',
+          to: '/zip'
         }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'H-Tools'
     }
   }
 }
---
+++ pages/zip.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <form action>
+        <input id="zip" type="file" webkitdirectory directory multiple />
+      </form>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+  mounted() {
+    const zipInput = document.getElementById("zip");
+    zipInput.onchange = function() {
+      console.log(zipInput.file)
+    };
+  }
+};
+</script>
