96cdfab59c71b06fd78f3ad8cdb580698e2a06bb
Hakamate <thomas.clement@viacesi.fr>
2020-07-21 18:34:21 +0200

    can use adonis api

---
+++ pages/download.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <h1 class="my-3">Convert Youtube to MP3</h1>
+      <div class="d-flex justify-center">
+        <v-text-field id="url-input" label="Youtube URL"></v-text-field>
+        <v-btn id="convert-button" class="align-self-center ml-4" color="error"
+          >Convert</v-btn
+        >
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  components: {},
+  mounted() {
+    const urlInput = document.getElementById("url-input");
+    const convertBtn = document.getElementById("convert-button");
+
+    convertBtn.addEventListener("click", () => {
+      console.log(`URL: ${urlInput.value}`);
+      this.sendURL(urlInput.value);
+    });
+  },
+  methods:{
+    sendURL: async function(url){
+      console.log(url)
+      await axios.post(`http://127.0.0.1:3333/api/download`, {params:{URL:url}})
+        .catch(error => {
+          console.log(error);
+        })
+        .then(response => console.log(response))
+    }
+  }
+};
+</script>
+<style  scoped>
+
+#convert-button {
+  transform: translateY(-3px);
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to H-Tools, an Web App used to group NuxtApps
+        </v-card-title>
+        <v-card-text>
+          <h2>Functionalities : </h2>
+          <ul class="ma-3">
+            <li>Convert Youtube to MP4</li>
+          </ul>
+          <div class="text-xs-right">
+            <em><small>&mdash; Thomas CLEMENT (Hakamate)</small></em>
+          </div>
+          <hr class="my-3">
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            H-Tools GitHub
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/download"
+          >
+            Convert Ytb
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
